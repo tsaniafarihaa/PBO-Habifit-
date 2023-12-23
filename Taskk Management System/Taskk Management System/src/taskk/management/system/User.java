623dd9b075fc769bec5d5a95fccebd4e4baa935b
fiaannalia <90300374+fiaannalia@users.noreply.github.com>
2023-12-23 18:00:41 +0700

    Add files via upload

---
+++ Taskk Management System/Taskk Management System/src/taskk/management/system/User.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taskk.management.system;
+
+/**
+ *
+ * @author aisha
+ */
+public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {;
+        this.username = username;
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
